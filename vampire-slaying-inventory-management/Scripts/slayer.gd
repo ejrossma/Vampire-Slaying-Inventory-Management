4df5f6665cfd1a96d6dea528110f6dc3ff33e8f6
ejrossma <elyrossman@gmail.com>
2026-07-26 08:35:53 -0700

    Creating Drag and Drop and Fixed Menu

--- vampire-slaying-inventory-management/Scripts/slayer.gd
+++ vampire-slaying-inventory-management/Scripts/slayer.gd
@@ -51,3 +51,48 @@ func updateCard() -> void:
 	AgilityLabel.text = str(getStat("AGILITY"))
 	NameLabel.text = slayer_name
 	slayerSprite.texture = slayerTexture
+
+
+#drag and drop
+var draggable = false
+var is_inside_droppable = false
+var body_ref
+var offset : Vector2
+var initialPos : Vector2
+
+func _process(delta):
+	if draggable:
+		if Input.is_action_just_pressed("click"):
+			initialPos = global_position
+			offset = get_global_mouse_position() - global.position
+			global.is_dragging = true
+		
+		if Input.is_action_pressed("click"):
+			global_position = get_global_mouse_position() - offset
+		elif Input.is_action_just_released("click"):
+			global.is_dragging = false
+			var tween = get_tree().create_tween()
+			if is_inside_droppable:
+				tween.tween_property(self, "position", body_ref.position, 0.2).set_ease(Tween.EASE_OUT)
+			else:
+				tween.tween_property(self, "global_position", initialPos, 0.2).set_ease(Tween.EASE_OUT)
+
+func _on_area_2d_mouse_entered() -> void:
+	if not global.is_dragging:
+		draggable = true
+		scale = Vector2(1.05, 1.05)
+
+func _on_area_2d_mouse_exited() -> void:
+	if not global.is_dragging:
+		draggable = false
+		scale = Vector2(1,1)
+
+func _on_area_2d_body_entered(body: StaticBody2D) -> void:
+	if body.is_in_group("droppable"):
+		is_inside_droppable = true
+		body.modulate = Color(Color.GRAY, 0.85)
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("droppable") && body_ref == body:
+		is_inside_droppable = false
+		body.modulate = Color(Color.GRAY, 0.7)
