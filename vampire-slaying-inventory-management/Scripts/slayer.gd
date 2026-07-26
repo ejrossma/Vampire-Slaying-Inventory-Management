529bb29ee3bab49fb7d414aca1be439a2a026c34
ejrossma <elyrossman@gmail.com>
2026-07-25 21:10:00 -0700

    Updated Map

--- vampire-slaying-inventory-management/Scripts/slayer.gd
+++ vampire-slaying-inventory-management/Scripts/slayer.gd
@@ -8,8 +8,13 @@ var slayer_name : String
 @export var Agility : int
 @export var Intelligence : int
 
+@export var IntelligenceLabel : Label
+@export var StrengthLabel : Label
+@export var AgilityLabel : Label
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	updateStats()
 	pass # Replace with function body.
 
 
@@ -18,14 +23,12 @@ func _process(delta: float) -> void:
 	pass
 
 func getStat(stat : String) -> int:
-	match stat:
-		mission.Types.STRENGTH:
-			return Strength
-		mission.Types.AGILITY:
-			return Agility
-		mission.Types.INTELLIGENCE:
-			return Intelligence
-	return 0
+	if stat == "STRENGTH":
+		return Strength
+	elif stat == "AGILITY":
+		return Agility
+	else:
+		return Intelligence
 	
 func incrStat(stat: String) -> void:
 	match stat:
@@ -35,5 +38,10 @@ func incrStat(stat: String) -> void:
 			Agility += 1
 		mission.Types.INTELLIGENCE:
 			Intelligence += 1
+	updateStats()
 	return
 		
+func updateStats() -> void:
+	IntelligenceLabel.text = str(getStat("INTELLIGENCE"))
+	StrengthLabel.text = str(getStat("STRENGTH"))
+	AgilityLabel.text = str(getStat("AGILITY"))
