1d95e1cde0dc455d6d7ac25b699629dc9f02d294
longnut <jandra29@depaul.edu>
2026-07-26 08:37:38 -0700

    audio manager

---
+++ vampire-slaying-inventory-management/Scripts/audio_manager.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@export var mute: bool = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+		
+	pass # Replace with function body.
+
+func play_MainMenuMusic():
+		if not mute:
+				$MainMenuMusic.play()
+func stop_MainMenuMusic():
+		if not mute:
+				$MainMenuMusic.stop()
+func play_Pause():
+		if not mute:
+				$Pause.play()
+func play_Unpause():
+		if not mute:
+				$UnPause.play()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- vampire-slaying-inventory-management/Scripts/menu_scene.gd
+++ vampire-slaying-inventory-management/Scripts/menu_scene.gd
@@ -1,8 +1,10 @@
 extends CanvasLayer
 
 @export var instructionsPage : ColorRect
-
+func _ready():	
+	AudioManager.play_MainMenuMusic()
 func _on_button_pressed() -> void:
+	AudioManager.stop_MainMenuMusic()
 	get_tree().change_scene_to_file("res://Scenes/PlayScene.tscn")
 
 func _on_button_pressed_instructions() -> void:
